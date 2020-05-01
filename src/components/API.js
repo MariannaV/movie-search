c759cc0284704784af5115e285259a33f769353c
Mari <vergunmari@gmail.com>
2020-05-01 23:05:43 +0300

    feat: add slide creation

--- src/components/API.js
+++ src/components/API.js
@@ -4,6 +4,7 @@ export const API_OMDB = {
   },
   searchResults: new Map([]),
   movies: new Map([]),
+  moviesRaiting: new Map([]),
   //TODO: get search url params api mdn
   async moviesGetBySearch(params = {}) {
     const { text, year, page } = params;
@@ -18,15 +19,91 @@ export const API_OMDB = {
         this.movies.set(movie.imdbID, movie);
         movieIds.add(movie.imdbID);
       });
-      this.searchResults.set(text, movieIds);
-      if ('Error' in result) throw Error(result.Error);
-      console.log(result, text, page, pageSize, result.totalResults, result.Search);
+      if (!this.searchResults.has(text)) {
+        this.searchResults.set(text, new Set());
+      }
 
-      console.log(this.movies, this.searchResults);
+      movieIds.forEach((movieId) => {
+        this.searchResults.get(text).add(movieId);
+      });
+
+      if ('Error' in result) throw Error(result.Error);
+      console.log(
+        'result:',
+        result,
+        'text:',
+        text,
+        'page:',
+        page,
+        'pageSize:',
+        pageSize,
+        'totalResults:',
+        result.totalResults,
+        'result.Search:',
+        result.Search
+      );
+      // console.log('!!!', this.searchResults.get(text).add('123'));
+      console.log('movies:', this.movies, 'searchResults', this.searchResults);
     } catch (error) {
       console.log(error);
     }
   },
+  async moviesRaitingGetById(params = {}){
+    const { movieId } = params;
+    try {
+      const response = await fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=${this.apikey}`);
+      if (!response.ok) throw Error('NO OK');
+      const result = await response.json();
+      console.log(result.imdbRating);
+      if ('Error' in result) throw Error(result.Error);
+      // return result;
+      this.moviesRaiting.set(movieId, result.imdbRating);
+      console.log(this.moviesRaiting)
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
 };
 
-API_OMDB.moviesGetBySearch({ text: 'House', page: 1 /*year: -2*/ });
+
+
+window.onload = async () => {
+  await API_OMDB.moviesGetBySearch({ text: 'House', page: 1 /*year: -2*/ });
+  console.log(slidesCreate());
+  console.log(API_OMDB.moviesRaitingGetById({movieId: 'tt0180093'}))
+};
+
+
+// API_OMDB.moviesGetBySearch({ text: 'House', page: 2 /*year: -2*/ });
+
+function slidesCreate() {
+  // fragment = document.createDocumentFragment();
+  const slidesContainer = document.createElement('div');
+  slidesContainer.classList.add('glide', 'movies-slider');
+  API_OMDB.movies.forEach(function (slideData) {
+    slidesContainer.appendChild(slideCreate(slideData));
+  });
+  return slidesContainer;
+}
+
+function slideCreate(moviesData, moviesRaitingData) {
+  const slide = document.createElement('div');
+  slide.classList.add('glide__slide', 'card-item');
+  slide.insertAdjacentHTML(
+    'beforeend',
+    `
+               <div class='card-title'>
+                   <p>${moviesData.Title}</p>
+               </div>
+               <div class='card-img'>
+                    <img src=${moviesData.Poster}>
+                </div>
+               <div class='card-bottom'>
+                    <div class='year'>${moviesData.Year}</div>
+                    <div class='raiting'>${moviesRaitingData}</div>
+                </div>
+         `
+  );
+  return slide;
+}
