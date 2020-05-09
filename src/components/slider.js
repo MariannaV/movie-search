2c5eb99d03e3cbfac169760c4ae66147cf5e5c20
Mari <vergunmari@gmail.com>
2020-05-10 02:11:17 +0300

    feat: change the slider settings

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,6 +1,7 @@
 import Swiper from '/libs/swiper/js/swiper.esm.browser.bundle.js';
 import { API_OMDB, API_TRANSLATE } from './API.js';
 import { addSearchResults } from '../pages/common.js';
+
 export const movieSlider = sliderCreate({ sliderName: 'movieSlider' });
 
 function sliderCreate({ sliderName }) {
@@ -8,29 +9,27 @@ function sliderCreate({ sliderName }) {
   return {
     init() {
       this[sliderId] = new Swiper(`#${sliderName}`, {
+        height: 600,
         speed: 400,
+        updateOnWindowResize: true,
         breakpoints: {
           0: {
             slidesPerView: 1,
-            centeredSlides: true,
+            spaceBetween: 20,
           },
-          800: {
+          550: {
             slidesPerView: 2,
+            spaceBetween: 30,
           },
           900: {
             slidesPerView: 3,
           },
+          1250: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
         },
-        // centeredSlides: true,
-        centeredSlidesBounds: true,
-        // dynamicBullets: true,
-        spaceBetween: 10,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-          dynamicBullets: true,
-          dynamicMainBullets: 3,
-        },
+
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
@@ -68,15 +67,14 @@ function sliderCreate({ sliderName }) {
 
         addSearchResults(
           (() => {
-            if (isNotFound) return `No results for ${history.state.searchMovie}`;
-            if (isTranslated) return `Results for ${searchMovie}`;
+            if (isNotFound) return `No results for "${history.state.searchMovie}"`;
+            if (isTranslated) return `Showing results for "${searchMovie}"`;
             return null;
           })()
         );
 
         if (isNotFound) {
           history.replaceState(null, 'movies');
-          addSearchResults(`No results for ${searchMovie}`);
           this.slidesCreate();
           return;
         }
@@ -84,7 +82,7 @@ function sliderCreate({ sliderName }) {
       const searchMovieIds = [...getSearchResultsByCurrentSearchMovie()];
 
       const searchMovies = searchMovieIds.map((movieId) => API_OMDB.movies.get(movieId));
-      document.querySelector('.lds-spinner').classList.remove('loading');
+      document.querySelector('.loader-wrapper').classList.remove('loading');
       this.slider.appendSlide(
         searchMovies.slice((history.state.page - 1) * API_OMDB.pageSize).map((movieData) => this.slideCreate(movieData))
       );
@@ -96,9 +94,9 @@ function sliderCreate({ sliderName }) {
         'beforeend',
         `
                <div class='card-title'>
-                <a target="_blank" href="https://www.imdb.com/title/${moviesData.imdbID}/">
-                   <p>${moviesData.Title}</p>
-                   </a>
+                  <a target="_blank" href="https://www.imdb.com/title/${moviesData.imdbID}/">
+                     ${moviesData.Title}
+                  </a>
                </div>
                <div class='card-img'>
                     <img src=${
